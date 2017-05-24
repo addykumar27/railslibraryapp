3f6bed8543cd8f23e1baa777d522590c6bc7ebff
addykumar27 <adarshna27@yahoo.com>
2017-05-24 14:27:16 -0700

    adding show user routes,controller and views functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,10 @@ class UsersController < ApplicationController
 	@user = User.create(user_params)
     redirect_to root_path
 	end
-
+	
+	def show
+    @user = User.find_by_id(params[:id])
+  	end
 
 
 
