645ef401d84465c7d1e56bdb5dca03c13d49db5a
addykumar27 <adarshna27@yahoo.com>
2017-05-25 11:54:20 -0700

    adding join tables in the models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
 	has_secure_password
+	has_many :library_users, dependent: :destroy
+  	has_many :libraries, through: :library_users
+
 
 
 	def self.confirm(params)
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do #rails will list our routes.get '/' to get user
 
   get '/login', to: 'sessions#new'
   get '/logout', to: 'sessions#destroy'
-  post '/sessions', to: 'sessions#create'
+  post'/sessions', to: 'sessions#create'
   get 'libraries', to: 'libraries#index'
   get '/libraries/new', to: 'libraries#new', as: 'new_library'
+  post'/libraries', to: 'libraries#create'
+  get '/libraries/:id', to: 'libraries#show', as: 'library'
 end
